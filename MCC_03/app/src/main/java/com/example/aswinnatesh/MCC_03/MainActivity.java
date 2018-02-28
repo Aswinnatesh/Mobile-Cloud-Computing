0e75716611142e7227893167872237b5a6d711b6
Aswin's MAC <aswinnatesh.venkatesh@stonybrook.edu>
2018-02-27 19:51:04 -0500

    Added

---
+++ MCC_03/app/src/main/java/com/example/aswinnatesh/MCC_03/MainActivity.java
@@ -0,0 +1,49 @@
+package com.example.aswinnatesh.MCC_03;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class  MainActivity extends AppCompatActivity {
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    private static final int MY_PERMISSIONS_REQUEST_CODE = 2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(com.example.aswin.lab2a.R.layout.activity_main);
+        Button newcamera = (Button) this.findViewById(com.example.aswin.lab2a.R.id.button);
+        newcamera.setOnClickListener(new View.OnClickListener()
+
+                {
+                    @Override
+                    public void onClick(View view) {
+                        Intent cameraintent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                        if (cameraintent.resolveActivity(getPackageManager()) != null) {
+                            startActivityForResult(cameraintent, REQUEST_IMAGE_CAPTURE);
+                        }
+                    }
+
+
+
+        });
+    }
+
+      protected void onActivityResult(int requestCode, int resultCode, Intent data)
+      {
+          if(requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK)
+          {
+              Bundle extras = data.getExtras();
+              Bitmap image1 = (Bitmap) extras.get("data");
+              ImageView mImageView = (ImageView) findViewById(com.example.aswin.lab2a.R.id.mimage);
+              mImageView.setImageBitmap(image1);
+          }
+      }
+
+}
